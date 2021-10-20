b253c797734bad5b63b03d4670e8399147d4d650
Michael Clerx <michael.clerx@nottingham.ac.uk>
2021-10-20 16:20:47 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#
+# wevis setuptools script
+#
+from setuptools import setup, find_packages
+
+# Go!
+setup(
+    # Module name (lowercase)
+    name='wevis',
+    version='0.0.1',
+
+    # Description
+    description='Client/Server IO for "Where\'s Ben Nevis".',
+    #long_description=readme,
+    #long_description_content_type='text/markdown',
+
+    # License name
+    license='BSD 3-clause license',
+
+    # Maintainer information
+    # author='',
+    # author_email='',
+    maintainer='Michael Clerx',
+    maintainer_email='michael.clerx@nottingham.ac.uk',
+    url='https://github.com/CardiacModelling/wevis',
+
+    # Packages to include
+    packages=find_packages(include=('wevis', 'wevis.*')),
+
+    # Include non-python files (via MANIFEST.in)
+    #include_package_data=True,
+
+    # List of dependencies
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [
+            'flake8>=3',            # For code style checking
+        ],
+    },
+    python_requires='>=3.5',
+)
